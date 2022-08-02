0eb1dc7286e4c41da97cb54b217400d404154426
Paolo <plgrazon@gmail.com>
2022-08-02 10:31:12 -0400

    Time: 94 ms (63.47%), Space: 43.9 MB (87.07%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,15 +11,25 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-  if (list1 === null) {
-    return list2;
-  } else  if (list2 === null) {
-    return list1;
-  } else if (list1.val <= list2.val) {
-    list1.next = mergeTwoLists(list1.next, list2);
-    return list1;
-  } else {
-    list2.next = mergeTwoLists(list1, list2.next);
-    return list2;
+  let dummy = new ListNode(-1, null);
+  let prev = dummy;
+  
+  while(list1 && list2) {
+    if (list1.val <= list2.val) {
+      prev.next = list1;
+      list1 = list1.next;
+    } else {
+      prev.next = list2;
+      list2 = list2.next;
+    }
+    prev = prev.next;
   }
+  
+  if (list1 === null) {
+    prev.next = list2;
+  } else {
+    prev.next = list1;
+  }
+  
+  return dummy.next;
 };
