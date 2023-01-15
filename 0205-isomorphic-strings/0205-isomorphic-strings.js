c0969d4e6ed8e90b234ba45c3d5a8eed01651b0f
Paolo <plgrazon@gmail.com>
2023-01-15 11:37:43 +0800

    Time: 71 ms (93.85%), Space: 42.7 MB (67.99%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+  let sHash = {};
+  let tHash = {};
+  
+  if (s.length !== t.length) return false;
+  
+  for (let i = 0; i < s.length; i++) {
+    let sChar = s[i];
+    let tChar = t[i];
+        
+    if (!sHash.hasOwnProperty(sChar) && !tHash.hasOwnProperty(tChar)) {
+      sHash[sChar] = tChar;
+      tHash[tChar] = sChar;
+    } else if (sHash[sChar] !== tChar || tHash[tChar] !== sChar) {
+      return false;
+    } 
+  }
+  
+  console.log(sHash, tHash);
+  
+  return true;
+};
