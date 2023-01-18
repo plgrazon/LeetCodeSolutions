b978d840e1b15c7adff606e1c700210f3b458bef
Paolo <plgrazon@gmail.com>
2023-01-18 06:12:47 -0500

    Time: 78 ms (86.66%), Space: 45.1 MB (31.51%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+  const visitedNodes = new Set();
+  
+  while (!visitedNodes.has(head) && head) {
+    if (!visitedNodes.has(head)) {
+      visitedNodes.add(head);
+    } else {
+      return head;
+    }
+    
+    head = head.next;
+  }
+  
+  return head;
+};
