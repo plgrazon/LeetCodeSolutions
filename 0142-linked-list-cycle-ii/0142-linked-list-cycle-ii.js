f09dc7419c76f3fac8cd5c048bb3cd571dff8841
Paolo <plgrazon@gmail.com>
2023-01-18 13:04:33 -0500

    Time: 85 ms (68.72%), Space: 45.1 MB (38.22%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -10,7 +10,40 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+var getIntersection = function(head) {
+  let slow = head;
+  let fast = head;
+  
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    
+    if (slow === fast) {
+      return slow;
+    }
+  }
+  
+  return null;
+}
+
 var detectCycle = function(head) {
+  let intersection = getIntersection(head);
+  
+  if (!intersection) return null;
+  
+  let pt1 = head;
+  let pt2 = intersection;
+  
+  while (pt1 !== pt2) {
+    pt1 = pt1.next;
+    pt2 = pt2.next;
+  }
+  
+  return pt1;
+}
+
+var detectCycleLoop = function(head) {
   const visitedNodes = new Set();
   
   while (head) {
