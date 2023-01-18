50dff8580fa914a6e3611c9753a63cf14c86726f
Paolo <plgrazon@gmail.com>
2023-01-18 04:32:46 -0500

    Time: 62 ms (84.64%), Space: 42.4 MB (13.29%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -10,10 +10,22 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
+  let stack = [];
+  let currNode = head;
+  
+  while (currNode) {
+    stack.push(currNode);
+    currNode = currNode.next;
+  }
+  
+  return stack[Math.floor(stack.length / 2)];
+}
+
+var middleNodeLoop = function(head) {
   let slow = head;
   let fast = head;
   
-  while (fast !== null && fast.next !== null) {
+  while (fast && fast.next) {
     slow = slow.next;
     fast = fast.next.next;
   }
