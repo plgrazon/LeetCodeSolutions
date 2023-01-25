f6c60ce9c6eb48eb3f0dc58c18dbac2cfbe6fa51
Paolo <plgrazon@gmail.com>
2023-01-25 11:36:53 -0500

    Time: 72 ms (97.42%), Space: 44.7 MB (75.00%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -0,0 +1,32 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+  const height = grid.length;
+  const width = grid[0].length;
+  let islands = 0;
+  
+  const dfs = (row, col) => {
+    if (row < 0 || row >= height || col < 0 || col >= width || grid[row][col] === "0") return;
+    
+    grid[row][col] = "0";
+    
+    dfs(row + 1, col);
+    dfs(row - 1, col);
+    dfs(row, col + 1);
+    dfs(row, col - 1);
+  }
+  
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      if (grid[row][col] === "1") {
+        islands++;
+        
+        dfs(row, col);
+      }
+    }
+  }
+  
+  return islands;
+};
