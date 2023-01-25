6428755ae03ddfb31fbd31eb5eef99964ab011ba
Paolo <plgrazon@gmail.com>
2023-01-25 09:26:08 -0500

    Time: 103 ms (38.25%), Space: 44.4 MB (35.74%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, newColor) {
+  const height = image.length;
+  const width = image[0].length;
+  const ogColor = image[sr][sc];
+  
+  const dfs = (row, col, currColor) => {
+    if (row < 0 || row >= height || col < 0 || col >= width 
+        || image[row][col] === newColor || image[row][col] !== ogColor) {
+      return;
+    } else {
+      image[row][col] = newColor;
+      
+      dfs(row + 1, col, currColor);
+      dfs(row - 1, col, currColor);
+      dfs(row, col + 1, currColor);
+      dfs(row, col - 1, currColor);
+      
+      return;
+    }
+  }
+  
+  dfs(sr, sc, ogColor);
+  
+  return image;
+};
