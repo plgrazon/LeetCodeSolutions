1693f09b4c31d2c82e11387b84e4fa3fd33a480e
Paolo <plgrazon@gmail.com>
2023-01-28 12:45:44 -0500

    Time: 72 ms (91.01%), Space: 44.5 MB (48.92%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -2,8 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-
 var pivotIndex = function(nums) {
+  let max = 0;
+  let leftSum = 0;
+  let rightSum = 0;
+  
+  for (let num of nums) max += num;
+  for (let i = 0; i < nums.length; i ++) {
+    rightSum = max - leftSum - nums[i];
+    
+    if (leftSum === rightSum) return i;
+    
+    leftSum += nums[i];
+  }
+  
+  return -1;
+}
+
+var pivotIndexLoop = function(nums) {
   for (let i = 1; i < nums.length; i++) {
     nums[i] += nums[i - 1];
   }
