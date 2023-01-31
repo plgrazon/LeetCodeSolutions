3ba68fb1c0f61593f7662ad32dfbda4a018f56ca
Paolo <plgrazon@gmail.com>
2023-01-31 17:11:52 -0500

    Time: 73 ms (73.21%), Space: 44.4 MB (35.47%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,40 +11,23 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-  if (list1 === null) {
-    return list2;
-  } else if (list2 === null) {
-    return list1;
-  } else if (list1.val <= list2.val) {
-    list1.next = mergeTwoLists(list1.next, list2);
-    return list1;
-  } else {
-    list2.next = mergeTwoLists(list1, list2.next);
-    return list2;
-  }
-};
-
-var mergeTwoListsLoop = function(list1, list2) {
-  const head = new ListNode(-1);
-  let currNode = head;
-  
-  while (list1 && list2) {
-    if (list1.val <= list2.val) {
-      currNode.next = list1;
-      list1 = list1.next;
-    } else {
-      currNode.next = list2;
-      list2 = list2.next;
-    }
+    const dummy = new ListNode(-1);
+    let head = dummy;
     
-    currNode = currNode.next;
-  }
-  
-  if (list1 === null) {
-    currNode.next = list2;
-  } else {
-    currNode.next = list1;
-  }
-  
-  return head.next;
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            head.next = list1;
+            list1 = list1.next;
+        } else if (list1.val > list2.val) {
+            head.next = list2;
+            list2 = list2.next;
+        }
+
+        head = head.next;
+    }
+
+    if (list1 === null) head.next = list2;
+    else if (list2 === null) head.next = list1;
+
+    return dummy.next;
 };
