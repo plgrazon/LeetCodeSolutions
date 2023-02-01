0ae50ab6f469bb621789bf8f675752b1f57b7707
Paolo <plgrazon@gmail.com>
2023-01-31 23:35:13 -0500

    Time: 63 ms (92.35%), Space: 43.4 MB (83.67%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.js
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.js
@@ -5,9 +5,26 @@
 var minOperations = function(nums) {
     let operations = 0;
 
-    if (nums.length === 1) {
-        return operations;
+    if (nums.length === 1) return operations;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        let prevNum = nums[i];
+        let nextNum = nums[i + 1];
+
+        if (nextNum <= prevNum) {
+            let diff = prevNum - nextNum + 1;
+            nums[i + 1] += diff;
+            operations += diff;
+        }
     }
+
+    return operations;
+}
+
+var minOperationsLoop = function(nums) {
+    let operations = 0;
+
+    if (nums.length === 1) return operations;
 
     for (let i = 0; i < nums.length - 1; i++) {
         let prevNum = nums[i];
