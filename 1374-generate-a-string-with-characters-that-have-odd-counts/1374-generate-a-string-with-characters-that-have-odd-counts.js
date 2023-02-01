ec0d7c7f832793f67b87296a385b7d9cfb4b4bf2
Paolo <plgrazon@gmail.com>
2023-01-31 22:57:06 -0500

    Time: 59 ms (86.55%), Space: 41.8 MB (85.71%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
@@ -5,11 +5,6 @@
 var generateTheString = function(n) {
     const isEven = n % 2 === 0;
     const length = isEven ? n - 1 : n
-    let str = '';
 
-    for (let i = 0; i < length; i++) {
-        str += 'a';
-    }
-
-    return isEven ? str + 'b' : str;
+    return isEven ?  `${'a'.repeat(length)}b` : `${'a'.repeat(length)}`;
 };
