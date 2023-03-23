7cd66e48765f37028ba6f4d2b8448bdf0ddd9cb6
Paolo <plgrazon@gmail.com>
2023-03-23 12:40:08 -0400

    Time: 76 ms (67.42%), Space: 45.6 MB (75.49%) - LeetHub

--- 0136-single-number/0136-single-number.ts
+++ 0136-single-number/0136-single-number.ts
@@ -6,4 +6,19 @@ function singleNumber(nums: number[]): number {
   }
 
   return acc;
+}
+
+function singleNumberHash(nums: number[]): number {
+  let hash = {};
+
+  for (let num of nums) {
+    if (hash?.[num]) hash[num]++;
+    else hash[num] = 1;
+  }
+
+  for (let key in hash) {
+    if (hash[key] === 1) return Number(key);
+  }
+
+  return 1;
 };
