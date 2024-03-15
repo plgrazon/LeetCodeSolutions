632ee08bc975a6705b58b6df3a1f2b2e4553f428
Paolo <plgrazon@gmail.com>
2024-03-15 16:10:48 -0400

    Time: 52 ms (85.73%) | Memory: 51.2 MB (86.13%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.ts
+++ 88-merge-sorted-array/merge-sorted-array.ts
@@ -2,19 +2,18 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-  let p1 = 0;
-  let p2 = 0;
+  let p1 = m - 1;
+  let p2 = n - 1;
 
-  let nums1copy = nums1.slice(0, m);
-
-  for (let writer = 0; writer < nums1.length; writer++) {  
-    if (p2 >= n || p1 < m && nums1copy[p1] < nums2[p2]) {
-      console.log(nums1copy[p1], nums2[p2])
-      nums1[writer] = nums1copy[p1];
-      p1++;
+  for (let writer = m + n - 1; writer >= 0; writer--) {
+    if (p2 < 0) break;
+    
+    if (nums1[p1] >= nums2[p2]) {
+      nums1[writer] = nums1[p1];
+      p1--;
     } else {
       nums1[writer] = nums2[p2];
-      p2++;
+      p2--;
     }
   }
 };
