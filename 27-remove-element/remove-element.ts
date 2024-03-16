8c9ebc46898663038ab60d73c2e4b5f4f7ee2a0c
Paolo <plgrazon@gmail.com>
2024-03-16 13:15:15 -0400

    Time: 57 ms (62.84%) | Memory: 51.7 MB (22.62%) - LeetSync

--- 27-remove-element/remove-element.ts
+++ 27-remove-element/remove-element.ts
@@ -1,17 +1,15 @@
 function removeElement(nums: number[], val: number): number {
-  let fast = 0;
-  let slow = 0;
-  const len = nums.length;
+  let start: number = 0;
+  let end: number = nums.length - 1;
 
-  while (fast < len) {
-    if (nums[fast] === val) {
-      fast++;
+  while (start <= end) {
+    if (nums[start] === val) {
+      [nums[start], nums[end]] = [nums[end], nums[start]];
+      end--;
     } else {
-      [nums[slow], nums[fast]] = [nums[fast], nums[slow]];
-      fast++;
-      slow++;
+      start++;
     }
   }
 
-  return slow;
+  return end + 1;
 };
