5dd17f3eb5ff384ce66621c028f73aca2514c32c
Paolo <plgrazon@gmail.com>
2024-03-20 09:41:42 -0400

    Time: 55 ms (73.39%) | Memory: 51.5 MB (52.10%) - LeetSync

--- 27-remove-element/remove-element.ts
+++ 27-remove-element/remove-element.ts
@@ -1,19 +1,38 @@
 function removeElement(nums: number[], val: number): number {
   // fast and slow pointer:
   // time: O(n)
-  // space: O(n)
-  let fast = 0;
-  let slow = 0;
+  // space: O(1)
+  // let fast = 0;
+  // let slow = 0;
 
-  while (fast < nums.length) {
-    if (nums[fast] === val) {
-      fast++;
+  // while (fast < nums.length) {
+  //   if (nums[fast] === val) {
+  //     fast++;
+  //   } else {
+  //     [nums[slow], nums[fast]] = [nums[fast], nums[slow]];
+  //     fast++;
+  //     slow++;
+  //   }
+  // }
+  //
+  // return slow;
+  //
+  // swap ends
+  // time: O(n)
+  // space: O(1)
+  let len = nums.length;
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start < len) {
+    if (nums[start] === val) {
+      [nums[start], nums[end]] = [nums[end], nums[start]];
+      end--;
+      len--;
     } else {
-      [nums[slow], nums[fast]] = [nums[fast], nums[slow]];
-      fast++;
-      slow++;
+      start++;
     }
   }
 
-  return slow;
+  return len;
 };
