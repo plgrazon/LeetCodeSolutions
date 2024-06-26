0e221faf4dccf5bfe2b0409fe926d329316a2092
Paolo <plgrazon@gmail.com>
2024-06-26 18:55:40 -0400

    Time: 68 ms (47.00%) | Memory: 54 MB (47.27%) - LeetSync

---
+++ 169-majority-element/majority-element.ts
@@ -0,0 +1,22 @@
+function majorityElement(nums: number[]): number {
+  const hash: { [key: string]: number } = {};
+  let max = 0;
+  let num = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    const curr = nums[i];
+
+    if (hash[curr]) {
+      hash[curr] += 1;
+    } else {
+      hash[curr] = 1;
+    }
+
+    if (hash[curr] > max) {
+      max = hash[curr];
+      num = curr;
+    }
+  }
+
+  return num;
+};
